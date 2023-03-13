e5632a39bb190c38e16d9f19f9ba2b20688a11b1
Leo Wong <leoelicos@gmail.com>
2023-03-14 04:32:48 +1100

    Heroku configuration for process.env.PORT

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(routes)
 
 db.once('open', () => {
-  app.listen(PORT, () => {
+  app.listen(process.env.PORT || PORT, () => {
     console.log(`API server  running on port ${PORT}!`)
   })
 })
