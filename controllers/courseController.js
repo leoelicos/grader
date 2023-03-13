2bd6b1c6d71f93e476e1dcef099ecf7066954f0d
Leo Wong <leoelicos@gmail.com>
2023-03-14 03:42:18 +1100

    Refactor to use async await

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,55 +1,77 @@
-const { Course, Student } = require('../models');
+const { Student, Course } = require('../models')
 
 module.exports = {
   // Get all courses
-  getCourses(req, res) {
-    Course.find()
-      .then((courses) => res.json(courses))
-      .catch((err) => res.status(500).json(err));
+  async getCourses(req, res) {
+    try {
+      const courses = await Course.find() //
+      // .populate('students')
+      res.json(courses)
+    } catch (e) {
+      console.error(e)
+      res.status(500).json(e)
+    }
   },
   // Get a course
-  getSingleCourse(req, res) {
-    Course.findOne({ _id: req.params.courseId })
-      .select('-__v')
-      .then((course) =>
-        !course
-          ? res.status(404).json({ message: 'No course with that ID' })
-          : res.json(course)
-      )
-      .catch((err) => res.status(500).json(err));
+  async getSingleCourse(req, res) {
+    try {
+      const course = await Course.findOne({ _id: req.params.courseId }).select('-__v')
+      if (!course) {
+        res.status(404).json({ message: 'No course with that ID' })
+        return
+      }
+      res.json(course)
+    } catch (e) {
+      console.error(e)
+      res.status(500).json(err)
+    }
   },
   // Create a course
-  createCourse(req, res) {
-    Course.create(req.body)
-      .then((course) => res.json(course))
-      .catch((err) => {
-        console.log(err);
-        return res.status(500).json(err);
-      });
+  async createCourse(req, res) {
+    try {
+      const course = await Course.create(req.body)
+      res.json(course)
+    } catch (e) {
+      console.error(e)
+      res.status(500).json(err)
+    }
   },
   // Delete a course
-  deleteCourse(req, res) {
-    Course.findOneAndDelete({ _id: req.params.courseId })
-      .then((course) =>
-        !course
-          ? res.status(404).json({ message: 'No course with that ID' })
-          : Student.deleteMany({ _id: { $in: course.students } })
-      )
-      .then(() => res.json({ message: 'Course and students deleted!' }))
-      .catch((err) => res.status(500).json(err));
+  async deleteCourse(req, res) {
+    try {
+      const course = await Course.findOneAndDelete({ _id: req.params.courseId })
+
+      if (!course) {
+        res.status(404).json({ message: 'No course with that ID' })
+        throw 'No course with that ID'
+      }
+
+      await Student.deleteMany({ _id: { $in: course.students } })
+
+      res.json({ message: 'Course and students deleted!' })
+    } catch (e) {
+      console.error(e)
+    }
   },
   // Update a course
-  updateCourse(req, res) {
-    Course.findOneAndUpdate(
-      { _id: req.params.courseId },
-      { $set: req.body },
-      { runValidators: true, new: true }
-    )
-      .then((course) =>
-        !course
-          ? res.status(404).json({ message: 'No course with this id!' })
-          : res.json(course)
+  async updateCourse(req, res) {
+    try {
+      const course = await Course.findOneAndUpdate(
+        //
+        { _id: req.params.courseId },
+        { $set: req.body },
+        { runValidators: true, new: true }
       )
-      .catch((err) => res.status(500).json(err));
-  },
-};
+
+      if (!course) {
+        res.status(404).json({ message: 'No course with that ID' })
+        throw 'No course with that ID'
+      }
+
+      res.json(course)
+    } catch (e) {
+      console.error(e)
+      res.status(500).json(e)
+    }
+  }
+}
