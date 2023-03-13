03ea7009b0badf1d093bd32e1b58ca074a4a602a
Leo Wong <leoelicos@gmail.com>
2023-03-14 04:19:02 +1100

    replace require with import, module.exports with export

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,5 @@
-const { connect, connection } = require('mongoose')
+import mongoose from 'mongoose'
+const { connect, connection } = mongoose
 
 // After you create your Heroku application, visit https://dashboard.heroku.com/apps/ select the application name and add your Atlas connection string as a Config Var
 // Node will look for this environment variable and if it exists, it will use it. Otherwise, it will assume that you are running this application locally
@@ -9,4 +10,4 @@ connect(connectionString, {
   useUnifiedTopology: true
 })
 
-module.exports = connection
+export default connection
