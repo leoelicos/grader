8a1ac3df9425befa0f6e7ef5d15e319fcb184a0a
Leo Wong <leoelicos@gmail.com>
2023-03-14 03:46:59 +1100

    Fix seed and remove console tables

--- utils/seed.js
+++ utils/seed.js
@@ -1,51 +1,40 @@
-const connection = require('../config/connection');
-const { Course, Student } = require('../models');
-const { getRandomName, getRandomAssignments } = require('./data');
+const connection = require('../config/connection')
+const { Course, Student } = require('../models')
 
-connection.on('error', (err) => err);
+const { getRandomName, getRandomAssignments } = require('./data')
+const mongoose = require('mongoose')
+const ObjectId = mongoose.Types.ObjectId
+connection.on('error', (err) => err)
 
 connection.once('open', async () => {
-  console.log('connected');
+  try {
+    await Course.deleteMany({})
+    await Student.deleteMany({})
+    const studentIds = []
+    for (let i = 0; i < 20; i++) {
+      const fullName = getRandomName()
+      const { insertedId } = await Student.collection.insertOne(
+        {
+          first: fullName.split(' ')[0],
+          last: fullName.split(' ')[1],
+          github: `${fullName.split(' ')[0]}${Math.floor(Math.random() * (99 - 18 + 1) + 18)}`,
+          assignments: getRandomAssignments(20)
+        },
+        { new: true }
+      )
+      studentIds.push(insertedId)
+    }
 
-  // Drop existing courses
-  await Course.deleteMany({});
+    await Course.collection.insertOne({
+      courseName: 'UCLA',
+      inPerson: false,
+      students: studentIds
+    })
 
-  // Drop existing students
-  await Student.deleteMany({});
-
-  // Create empty array to hold the students
-  const students = [];
-
-  // Loop 20 times -- add students to the students array
-  for (let i = 0; i < 20; i++) {
-    // Get some random assignment objects using a helper function that we imported from ./data
-    const assignments = getRandomAssignments(20);
-
-    const fullName = getRandomName();
-    const first = fullName.split(' ')[0];
-    const last = fullName.split(' ')[1];
-    const github = `${first}${Math.floor(Math.random() * (99 - 18 + 1) + 18)}`;
-
-    students.push({
-      first,
-      last,
-      github,
-      assignments,
-    });
+    console.info('Seeding complete! 🌱')
+  } catch (e) {
+    console.error(e)
+  } finally {
+    process.exit(0)
   }
-
-  // Add students to the collection and await the results
-  await Student.collection.insertMany(students);
-
-  // Add courses to the collection and await the results
-  await Course.collection.insertOne({
-    courseName: 'UCLA',
-    inPerson: false,
-    students: [...students],
-  });
-
-  // Log out the seed data to indicate what should appear in the database
-  console.table(students);
-  console.info('Seeding complete! 🌱');
-  process.exit(0);
-});
+})
